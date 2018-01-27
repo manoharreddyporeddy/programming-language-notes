b82bd4ebb2b7ee42e32f501c93476df5c3a3635b
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2018-01-27 11:01:23 +0530

    Update my-nodejs-notes.js

--- my-nodejs-notes.js
+++ my-nodejs-notes.js
@@ -1,1 +1,20 @@
 
+for (var i = 1; i < 5; i++){
+    setTimeout(() => { console.log(i); }, 1000);
+}
+
+for (let i = 1; i < 5; i++){
+    setTimeout(() => { console.log(i); }, 1000);
+}
+
+
+const locks assignment
+doesnot make immutable
+
+
+...
+	spreads both array and object elements, can be used in wrapper [] and {}
+	function (...a) {
+		console.log(a);
+	}
+
