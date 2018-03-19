27456750a087d70b60023d69803f1c7c7006416d
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2018-03-19 07:35:42 +0530

    more on manifest

--- my-android-programming-notes.java
+++ my-android-programming-notes.java
@@ -8,3 +8,15 @@
 // Sunshine Code
 //     https://github.com/udacity/ud851-Sunshine
 //     This is the main project
+
+
+// Activity
+//    Simple GUI/ graphical interface similar to Watsapp, etc.
+//    
+// \app\src\main\AndroidManifest.xml
+// This is manifest file
+/*
+manifest
+- application (this is the application, it has icon, theme, label/applciation name like Watsapp that display on top of app)
+-- activity (This is the our GUI, we can have our controls in this, like Button, Text box, etc.)
+*/
