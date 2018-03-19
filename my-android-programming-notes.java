218176a95f67cd2f33fa6f5606f25441da7d850e
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2018-03-19 07:31:46 +0530

    links to another program

--- my-android-programming-notes.java
+++ my-android-programming-notes.java
@@ -1,1 +1,10 @@
-my-android-programming-notes.java
+
+// Below program is created by Google & Udacity.
+
+// Toy App
+//     Link: https://github.com/udacity/ud851-Exercises
+//     This are sample excercises that will help to the main project
+
+// Sunshine Code
+//     https://github.com/udacity/ud851-Sunshine
+//     This is the main project
