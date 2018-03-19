4bd790cd071a724bf205dbea9c2225b5691ead95
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2018-03-19 07:54:55 +0530

    layouts & controls initial

--- my-android-programming-notes.java
+++ my-android-programming-notes.java
@@ -24,9 +24,24 @@ manifest
 -- activity (This is the our GUI, we can have our controls in this, like Button, Text box, etc.)
 
 RESOURCE FOLDER:
-    \app\src\main\res
-    This is the resource folder, it has other folders.
-    Like layout folder, menu folder, values folder, etc.
+  \app\src\main\res
+  This is the resource folder, it has other folders
+  Like layout folder, menu folder, values folder, etc.
 
+RES - LAYOUTS
+  res\layout
+    This has xmls that are layouts
+  res\layout\activity_main.xml
+    This is a layout file. (tough it says activity)
+  There are many layouts like FrameLayout, which is simple a simple one, it occupies the whole area of activity(?)
+  it generally is given layout_width & layout_height as "match_parent"
+
+  Each Layout can contain other controls like TextView
+
+CONTROLS
+  TextView - this is to view text that you have like a status of something happening or done, to notify user
+    generally has layout_width & layout_height as "wrap_content"
+    and other attributes like padding say as "16dp" & textSize say as "20sp"
+  
 
 */
