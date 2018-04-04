0b13ae4aa98a51bcb5302461a1da08be276edf04
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2018-04-04 19:12:02 +0530

    Update my-regexp-programming-notes.js

--- my-regexp-programming-notes.js
+++ my-regexp-programming-notes.js
@@ -1,5 +1,8 @@
 /*
 
+NOTE: these are not 100%, though they may be. :-)
+
+
 /^\d{4}$/
   4 digits
   
@@ -16,11 +19,16 @@
   2 digits, a hypen, then a 4 more digits
 
 /^[a-zA-Z]{2}$/
-  2 characters
+  2 alphabets
 
 /^\d{3}(\d{2})?$/
-  3 or 5 characters   -- (second 2 are optional)
+  3 or 5 alphabets   -- (second 2 are optional)
 
+/^\d{4} [a-zA-Z]{2}$/
+  4 digits, a space, then 2 alphabets
+
+/^[a-zA-Z]\d[a-zA-Z] \d[a-zA-Z]\d$/
+  an alphabet, then a digit, then another alphabet, then a space, then a digit, then an alphabet, then another digit
 
 ^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$
   look ahead to have a number
