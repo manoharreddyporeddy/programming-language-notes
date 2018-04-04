8dcbe917ee00fc9eb3cec88e8ddf7e8c57427faa
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2018-04-04 19:09:49 +0530

    Update my-regexp-programming-notes.js

--- my-regexp-programming-notes.js
+++ my-regexp-programming-notes.js
@@ -12,9 +12,14 @@
 /^\d{3} \d{2}$/
   3 digits, a space, then a 2 more digits
 
-/^\d{3}-\d{4}$/
-  3 digits, a hypen, then a 2 more digits
+/^\d{2}-\d{4}$/
+  2 digits, a hypen, then a 4 more digits
 
+/^[a-zA-Z]{2}$/
+  2 characters
+
+/^\d{3}(\d{2})?$/
+  3 or 5 characters   -- (second 2 are optional)
 
 
 ^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$
