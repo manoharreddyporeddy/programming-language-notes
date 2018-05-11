89b3e277f338666675d0b93c334ac19c73f51ea4
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2018-05-11 21:16:17 +0530

    Update my-reactjs-programming-notes.js

--- my-reactjs-programming-notes.js
+++ my-reactjs-programming-notes.js
@@ -1,1 +1,14 @@
 
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// production build
+//		https://reactjs.org/docs/optimizing-performance.html#use-the-production-build
+
+// https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi?hl=en
+// https://addons.mozilla.org/en-US/firefox/addon/react-devtools/
+
+// https://reactjs.org/docs/react-api.html#createelement
+// 		https://babeljs.io/repl/#?presets=react&code_lz=DwEwlgbgBAxgNgQwM5IHIILYFMC8AiJACwHsAHUsAOwHMBaOMJAFzwD4AoKKYQgRlYDKJclWpQAMoyZQAZsQBOUAN6l5ZJADpKmLAF9gAej4cuwAK5wTXbg1YBJSswTV5mQ7c7XgtgOqEETEgAguTuYFamtgDyMBZmSGFWhhYchuAQrADc7EA
+// 		https://codepen.io/gaearon/pen/VbbVLg?editors=0010
