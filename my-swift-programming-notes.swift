2c73a111c9b96f5cfee7416c79e2959e52a4f717
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2018-05-30 23:58:35 +0530

    Create my-swift-programming-notes.swift

---
+++ my-swift-programming-notes.swift
@@ -0,0 +1,38 @@
+<pre>
+
+Changes in swift versions
+	2.3 https://github.com/apple/swift-evolution/blob/master/releases/swift-2_2.md
+	3.0 https://github.com/apple/swift-evolution/blob/master/releases/swift-3_0.md
+	4.0 https://github.com/apple/swift-evolution/blob/master/releases/swift-4_0.md
+
+objective c to swift 3
+	https://developer.apple.com/swift/blog/
+	https://www.toptal.com/swift/from-objective-c-to-swift
+	http://www.tothenew.com/blog/12-key-differences-between-objective-c-and-swift/
+	https://www.infoworld.com/article/2920333/mobile-development/swift-vs-objective-c-10-reasons-the-future-favors-swift.html
+
+swift 4.1 official
+	tour: https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/GuidedTour.html#//apple_ref/doc/uid/TP40014097-CH2-ID1
+	guide: https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html#//apple_ref/doc/uid/TP40014097-CH5-ID309
+	reference: https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/AboutTheLanguageReference.html#//apple_ref/doc/uid/TP40014097-CH29-ID345
+
+swift 4:
+	https://www.tutorialspoint.com/swift/index.htm
+
+swift (no version given) - short tutorial
+	https://www.w3schools.in/swift-tutorial/
+
+swift 2 to 4 code conversion
+	Convert code from Swift 2.0 to 3.2 using Xcode 8
+	then continue with Xcode 9 (Swift 3.2 or 4).
+				https://stackoverflow.com/questions/44405819/code-migration-from-swift-2-x-to-swift-4
+
+online swift compilers
+	Swiftstub.com
+	Runswiftlang.com
+	iswift.org
+   
+books & others
+	https://ios.topcoder.com/learn
+
+</pre>
