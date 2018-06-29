67807ba56210cf4e2376b667be89aedb7a4fd241
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-06-29 17:01:26 +0530

    null propagator  example

---
+++ my-c#/my-csharp-examples/null-propagator/null-propagator/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace null_propagator
+{
+    class PriceBreak // (int min, int max, double price)
+    {
+        static int min = 0;
+        static int max = 100;
+        static double price = 10;
+
+        //PriceBreak(int min, int max, double price) { this.min = min; this.max = max; this.price = price; }
+
+        public int MinQuantity { get; } = min;
+        public int MaxQuantity { get; } = max;
+        public double Price { get; } = price;
+    }
+
+    class Product // (IList<PriceBreak> priceBreaks)
+    {
+        static List<PriceBreak> priceBreaks = null;
+        public Product(List<PriceBreak> priceBreaks1)
+        {
+            priceBreaks = priceBreaks1;
+        }
+        public IList<PriceBreak> PriceBreaks { get; } = priceBreaks;
+    }
+    class Program
+    {
+        delegate int NumberChanger(int n);
+        public delegate int MyDelegate(string s);
+
+        static int add(string s)
+        {
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Product p = new Product(null);
+
+            // bad
+            var minPrice1 = p.PriceBreaks[0].Price;
+
+            // OR
+
+            // good
+            double minPrice2 = 0;
+            if (p != null
+                && p.PriceBreaks != null
+                && p.PriceBreaks[0] != null)
+            {
+                minPrice2 = p.PriceBreaks[0].Price;
+            }
+
+            // better - new - null propagation operator(?) - double or null
+            var minPrice3 = p?.PriceBreaks?[0]?.Price;
+
+            // best - new - null propagation operator(?) & null coalescing operator (??) - double or 0
+            var minPrice4 = p?.PriceBreaks?[0]?.Price ?? 0;
+
+
+
+
+            // won't work
+            // var result1 = myDelegate ? ("someArgument");
+
+            // works
+            MyDelegate myDelegate = new MyDelegate(add);
+            var result2 = myDelegate?.Invoke("someArgument");
+
+        }
+    }
+}
---
+++ my-c#/my-csharp-examples/null-propagator/null-propagator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("null-propagator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("null-propagator")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb959609-42b9-4979-bf76-9f32fde009e6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
