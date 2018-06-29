98e92c86e8d8ad83e52eb76a0d2e4d260740ea3c
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-06-29 18:13:40 +0530

    nameof  example

---
+++ my-c#/my-csharp-examples/nameof-operator/nameof-operator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nameof_operator
+{
+    public class MyClass
+    {
+        public string MyProperty { get; set; }
+        public void MyFunc1() { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            Console.WriteLine(nameof(nameof_operator));         // nameof_operator
+            //
+            Console.WriteLine(nameof(MyClass));                 // MyClass
+            //
+            Console.WriteLine(nameof(MyClass.MyProperty));      // MyProperty
+            Console.WriteLine(nameof(MyClass.MyFunc1));         // MyFunc1
+        }
+    }
+}
