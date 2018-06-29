ae24867a1e5d7ea4f5770d15e8a4a1f5b0067f7b
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-06-29 18:59:53 +0530

    patial types & methods  example

---
+++ my-c#/my-csharp-examples/partial-types/partial-types/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partial_types
+{
+    // Partial Class
+    public partial class Employee
+    {
+        public void DoWork()
+        {
+        }
+    }
+    public partial class Employee
+    {
+        public void GoToLunch()
+        {
+        }
+    }
+
+    // partial method declaration
+    //
+    //// Definition in file1.cs  
+    //partial void onNameChanged();
+    //
+    //// Implementation in file2.cs  
+    //partial void onNameChanged()
+    //{
+    //    // method body  
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Partial Class
+            Employee e1 = new Employee();
+            e1.DoWork();
+            e1.GoToLunch();
+
+        }
+    }
+}
