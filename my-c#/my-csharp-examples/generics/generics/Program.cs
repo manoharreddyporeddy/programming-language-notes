2d3478c9c09b534e81bfd0b06dfc1a1682122627
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-06-29 18:55:35 +0530

    generics  example

---
+++ my-c#/my-csharp-examples/generics/generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics
+{
+   public class Class1 { }
+
+    // Declare the generic class.
+    public class GenericList<T>
+    {
+        public void Add(T input) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a list of type int, string & Class1.
+            GenericList<int> list1 = new GenericList<int>();            list1.Add(1);
+            GenericList<string> list2 = new GenericList<string>();      list2.Add("");
+            GenericList<Class1> list3 = new GenericList<Class1>();      list3.Add(new Class1());
+        }
+    }
+}
