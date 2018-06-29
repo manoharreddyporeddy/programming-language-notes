c25ad9899edb84bd37cf13c6b43fc0a96193f35b
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-06-29 20:15:50 +0530

    anonymous-methods 2  example

--- my-c#/my-csharp-examples/anonymous-methods/anonymous-methods/Program.cs
+++ my-c#/my-csharp-examples/anonymous-methods/anonymous-methods/Program.cs
@@ -8,8 +8,18 @@ namespace anonymous_methods
 {
     class Program
     {
+        delegate int NumberChangerDelegate(int n);
+
         static void Main(string[] args)
         {
+            NumberChangerDelegate twice = delegate (int n) { Console.Write("called GetTwice: "); return 2 * n; };
+            //
+            //      instead of
+            //
+            // static int GetTwice(int q) { Console.Write("called GetTwice: "); return 2 * q; }
+            // NumberChangerDelegate twice = new NumberChangerDelegate(GetTwice);
+
+            Console.WriteLine(twice(2));    // called GetTwice: 4
         }
     }
 }
