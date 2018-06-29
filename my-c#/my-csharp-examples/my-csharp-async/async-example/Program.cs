34e74bdcb7004f1b856dbe9d75f375f8d68cd14f
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-06-29 12:25:56 +0530

    async example

---
+++ my-c#/my-csharp-examples/my-csharp-async/async-example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace async_example
+{
+    class Program
+    {
+        static async Task<string> MyAsyncTaskFunction()
+        {
+            HttpClient httpc = new HttpClient();
+
+            Console.WriteLine("2 before await");
+            string a = await httpc.GetStringAsync("http://www.google.com"); ;
+            Console.WriteLine("3 after  await\n");
+
+            // Console.WriteLine(a);
+
+            return a;
+        }
+   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 before MyAsyncTaskFunction call");
+            var a = MyAsyncTaskFunction();
+            Console.WriteLine("4 after  MyAsyncTaskFunction call\n");
+
+            Console.WriteLine("5 before a.Result");
+            var b = a.Result;       // this line is important - waits till for async function to return
+            Console.WriteLine("6 after  a.Result\n");
+
+            // Console.WriteLine(b);
+        }
+    }
+}
