6d5441130ddccdd563f9981220f0e605ab0ca603
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-06-30 19:09:19 +0530

    interface example

---
+++ my-c#/my-csharp-examples/interface-example/interface-example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace interface_example
+{
+    // interface
+    //      is important since C# does not have multiple inheritance
+    interface ICar<T>
+    {
+        void Print();
+    }
+
+    // class implements interface
+    public class Car : ICar<Car>
+    {
+        string Name = "my car 1";
+
+        public void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car();
+            c1.Print();
+        }
+    }
+}
