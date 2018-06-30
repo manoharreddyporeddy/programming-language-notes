8482da0c5abec243f1f31f3e7ef691d319e57027
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-06-30 17:16:05 +0530

    partial methods  example

---
+++ my-c#/my-csharp-examples/partial-methods/partial-methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partial_methods
+{
+    partial class Program
+    {
+        // partial method
+        //      always returns void
+        //
+        //      This part can be in a separate file.
+        //      Comment out this method and the program will still compile.
+        static partial void OnSomethingHappened(String s)
+        {
+            Console.WriteLine($"Something happened: {s}");
+        }
+    }
+
+    partial class Program
+    {
+        // partial method
+        //      always returns void
+        static partial void OnSomethingHappened(string s);
+
+        static void Main(string[] args)
+        {
+            Program.OnSomethingHappened("error");
+        }
+    }
+}
