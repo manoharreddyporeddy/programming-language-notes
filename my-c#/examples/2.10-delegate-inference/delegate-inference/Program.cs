811518c4c95c13f75befa5482797230fb55e0fa1
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-07-03 00:12:08 +0530

    delegate inference  exampel

---
+++ my-c#/examples/2.10-delegate-inference/delegate-inference/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_inference
+{
+    delegate void MyDelegate(string msg);
+
+    class Program
+    {
+        public static void Greetings(string greet)
+        {
+            Console.WriteLine(greet);
+        }
+
+        static void Main(string[] args)
+        {
+            {
+                MyDelegate del = new MyDelegate(Greetings);
+                del("hello world");
+            }
+
+            {
+                MyDelegate del = Greetings;  // delegate inference - pass the method name and the delegate type would be inferred
+                del("hello world");
+            }
+        }
+    }
+}
