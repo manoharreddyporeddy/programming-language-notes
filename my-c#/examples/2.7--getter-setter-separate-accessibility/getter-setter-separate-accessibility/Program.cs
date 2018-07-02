7576879ae34b7737df111eb439f82d5c3926e904
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-07-02 22:12:01 +0530

    getter setter separate accessibility  examples

---
+++ my-c#/examples/2.7--getter-setter-separate-accessibility/getter-setter-separate-accessibility/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getter_setter_separate_accessibility
+{
+    class Employee
+    {
+        private string m_name;
+        public string Name
+        {
+            get { return m_name; }
+            protected set { m_name = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            // compile error
+            //      Error	CS0272	The property or indexer 'Employee.Name' cannot be used in this context because the set accessor is inaccessible
+            // emp.Name = "name 1";
+        }
+    }
+}
---
+++ my-c#/examples/2.7--getter-setter-separate-accessibility/getter-setter-separate-accessibility/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("getter-setter-separate-accessibility")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("getter-setter-separate-accessibility")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ce5d070-52de-4e64-9a7f-c29b027311be")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
