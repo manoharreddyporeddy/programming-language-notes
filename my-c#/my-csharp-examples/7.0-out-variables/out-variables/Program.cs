740c604b4ca994fdb27edba149a0d3dc2ae98d2c
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-07-02 08:45:58 +0530

    out variables

---
+++ my-c#/my-csharp-examples/7.0-out-variables/out-variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace out_variables
+{
+    class Program
+    {
+        static void GetName(out string name)
+        {
+            name = "some name";
+        }
+
+        static void Main(string[] args)
+        {
+            // old way
+            string name;
+            GetName(out name);
+
+            // new way - name2 is a out variable
+            GetName(out string name2);
+
+            Console.WriteLine($"name: {name}");
+            Console.WriteLine($"name2: {name2}");
+        }
+    }
+}
