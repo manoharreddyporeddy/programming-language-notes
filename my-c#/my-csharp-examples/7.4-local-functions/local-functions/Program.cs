224d9c82cb3cc5810b8f5e2c3e06262e4bb31d4a
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-07-02 10:26:19 +0530

    local functions  example

---
+++ my-c#/my-csharp-examples/7.4-local-functions/local-functions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace local_functions
+{
+    class Program
+    {
+        static void func1(int p)
+        {
+            Console.WriteLine("in func1");
+
+            // local variable
+            int x = p;
+
+
+            // local function
+            //      for encapsulation
+            //      can be defined in
+            //          another scope,
+            //          inside function, 
+            //          constructor,
+            //          inside a property (both getter and setter).
+            //      compiled to a private method
+            //      however can't be accessed by outside of the scope it is defined
+            void localFunc1(int n)
+            {
+                int n1 = n;
+
+                int x2 = x;     // can access enclosing block
+                int x3 = p;     // can access enclosing block
+
+                Console.WriteLine("in localFunc1");
+            }
+
+            // call local function
+            localFunc1(5);
+        }
+
+        static void Main(string[] args)
+        {
+            func1(1);
+        }
+    }
+}
