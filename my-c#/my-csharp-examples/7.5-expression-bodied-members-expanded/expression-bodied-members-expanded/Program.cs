49b6eb5d9e83c3945e4218976c9d852ff194698b
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-07-02 11:27:46 +0530

    expression bodied members  expanded to others  example

---
+++ my-c#/my-csharp-examples/7.5-expression-bodied-members-expanded/expression-bodied-members-expanded/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace expression_bodied_members_expanded
+{
+    // expanded to constructor, destructor
+    class ExpressionBodied1
+    {
+        // expression-bodied constructor
+        public ExpressionBodied1(string label) => this.Label = label;
+
+        // expression-bodied finalizer
+        ~ExpressionBodied1() => Console.Error.WriteLine("Finalized!");
+
+        private string label;
+
+        // expression-bodied get / set accessors.
+        public string Label
+        {
+            get => label;
+            set => this.label = value ?? "Default label";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpressionBodied1 o = new ExpressionBodied1("lable1");
+            Console.WriteLine(o.Label);
+        }
+    }
+}
