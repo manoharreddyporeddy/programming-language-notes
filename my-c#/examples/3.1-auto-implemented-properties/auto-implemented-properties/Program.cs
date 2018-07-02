ed19816359449f0c24f393770a3060645118a490
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-07-02 21:46:07 +0530

    auto implemented properties  examples

---
+++ my-c#/examples/3.1-auto-implemented-properties/auto-implemented-properties/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auto_implemented_properties
+{
+    class Program
+    {
+        //  compiler creates a
+        //          private,
+        //          anonymous backing field that
+        //              can only be accessed through the property's get and set accessors.
+
+        //            auto-implemented property
+        public string FirstName { get; set; }
+
+        // initialize auto-implemented property
+        public string FirstName2 { get; set; } = "Jane";
+
+        // auto-implemented properties
+        // read-only properties.
+        public string Name { get; }
+        public string Address { get; private set; }
+
+        // public constructor.   
+        public Program(string contactName, string contactAddress)
+        {
+            Name = contactName;
+            Address = contactAddress;
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program("contact1", "address1");
+            Console.WriteLine("done");
+        }
+    }
+}
