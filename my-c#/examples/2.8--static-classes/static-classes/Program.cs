b12481c89930f80b53c2f19a89b102ccde5c50ff
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-07-02 22:10:59 +0530

    static classes  examples

---
+++ my-c#/examples/2.8--static-classes/static-classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace static_classes
+{
+    // static class
+    static class StCls1
+    {
+        // can contain only static members
+        public static bool Func1(string str1)
+        {
+            Console.WriteLine(str1);
+            return true;
+        }
+
+        // Cannot contain Instance Constructors.
+        /* StCls1() {       }*/
+    }
+
+    // Is sealed
+    //class Derived : StCls1  {     }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StCls1.Func1("hello world");
+
+            // Cannot be instantiated.
+            //      StCls1 p = new StCls1();
+        }
+    }
+}
