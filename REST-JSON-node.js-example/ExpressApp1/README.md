c678763b89ee5bca9d22046b5c6f8f4b25e79772
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-07-25 18:01:24 +0530

    REST-JSON node.js example

---
+++ REST-JSON-node.js-example/ExpressApp1/README.md
@@ -0,0 +1,3 @@
+﻿# ExpressApp1
+
+
---
+++ REST-JSON-node.js-example/ExpressApp1/routes/users.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.send({ key1: 'value1', message: 'Hurray! Got a GET request for USERS resource.' });
+});
+
+router.post('/', function (req, res) {
+    res.send({ key1: 'value1', message: 'Hurray! Got a POST request for USERS resource.' });
+});
+
+router.put('/', function (req, res) {
+    res.send({ key1: 'value1', message: 'Hurray! Got a PUT request for USERS resource.' });
+});
+
+router.patch('/', function (req, res) {
+    res.send({ key1: 'value1', message: 'Hurray! Got a PATCH request for USERS resource.' });
+});
+
+module.exports = router;
