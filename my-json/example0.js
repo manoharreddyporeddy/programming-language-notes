4567aaadf853100fe5f495f60d16d1e248b7ad1c
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-08-07 18:57:31 +0530

    more json examples

---
+++ my-json/example0.js
@@ -0,0 +1,35 @@
+
+var obj = {
+
+    "myStr1": "string1",
+    "my    Str1": "string1",
+
+    "count1": 123,
+    "avg1": 123.45,
+    "bDone1": true,
+    "bDone2": false,
+    "prop1": null,
+    "arr1": [
+
+
+        "string1", 123, 123.45, true, false, null
+
+
+
+    ],
+    "obj1": {
+    },
+
+    "obj2": {
+        "myStr1": "string1",    /// members => collection of pair,     pair => string: value
+        "count1": 123,          //// object => { members }
+        "avg1": 123.45,
+        "bDone1": true,
+        "bDone2": false,
+        "prop1": null,
+        "arr1": ["string1", 123, 123.45, true, false, null],
+        "obj1": {
+        }
+    }
+
+}
