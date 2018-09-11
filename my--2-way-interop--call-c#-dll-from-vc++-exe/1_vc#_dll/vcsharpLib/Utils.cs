114cab98776cf91e969f618259499e949f844e6b
manoharreddyporeddy <manoharporeddy@outlook.com>
2018-09-11 09:07:50 +0530

    NativeMethods

--- my--2-way-interop--call-c#-dll-from-vc++-exe/1_vc#_dll/vcsharpLib/Utils.cs
+++ my--2-way-interop--call-c#-dll-from-vc++-exe/1_vc#_dll/vcsharpLib/Utils.cs
@@ -9,4 +9,15 @@ using System.Runtime.InteropServices;
 
 namespace ns_vcsharpLib
 {
+    static class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string dllToLoad);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeLibrary(IntPtr hModule);
+    }
 }
