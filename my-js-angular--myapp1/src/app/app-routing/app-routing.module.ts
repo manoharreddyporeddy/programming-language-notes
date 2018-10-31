271ad791617e3e25d2a2556cf2b43e0db3f5f491
MANOHAR REDDY POREDDY <maporedd@microsoft.com>
2018-10-31 22:14:10 +0530

    routes working

--- my-js-angular--myapp1/src/app/app-routing/app-routing.module.ts
+++ my-js-angular--myapp1/src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ListEmployeeComponent } from '../employee/list-employee/list-employee.component';
+import { CreateEmployeeComponent } from '../employee/create-employee/create-employee.component';
+
+
+const appRoutes: Routes = [
+  { path: 'empcreate', component: CreateEmployeeComponent },
+  { path: 'emplist', component: ListEmployeeComponent },
+  // { path: 'heroes', component: HeroListComponent, data: { title: 'Heroes List' } },
+  // { path: '**', component: PageNotFoundComponent },
+  { path: '', redirectTo: '/emplist', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(appRoutes)
+    // CommonModule
   ],
-  declarations: []
+  // declarations: [],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
