9e5e6b74c3e15230130d56366bd08442bbd19313
manoharreddyporeddy <manoharporeddy@outlook.com>
2019-02-27 10:51:38 +0530

    reuse Like button
    Signed-off-by: manoharreddyporeddy <manoharporeddy@outlook.com>

--- my-cordova/my-app/www/js/react/like_button.js
+++ my-cordova/my-app/www/js/react/like_button.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const e = React.createElement;
+// const e = React.createElement;
 
 class LikeButton extends React.Component {
   constructor(props) {
@@ -9,17 +9,29 @@ class LikeButton extends React.Component {
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+    if (this.state.liked) {   // when true
+      return 'You liked comment number ' + this.props.commentID; // this button changes to text
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+    // when false, renders as html button
+    return React.createElement(
+      'button', // html element
+      { onClick: () => this.setState({ liked: true }) },  // events
+      'Like'  // html element text/caption
     );
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    const commentID1 = parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(
+      React.createElement(LikeButton, { commentID: commentID1 }),   // render Like custom button
+      domContainer                                                  //  under div with class "like_button_container"
+    );
+  });
+
+// const domContainer = document.querySelector('#like_button_container');
+// ReactDOM.render(React.createElement(LikeButton), domContainer);
