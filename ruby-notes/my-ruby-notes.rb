9830d208cce2833615425b3b47ea8aeea3ad3658
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-04 20:17:17 +0530

    more notes

--- ruby-notes/my-ruby-notes.rb
+++ ruby-notes/my-ruby-notes.rb
@@ -176,6 +176,17 @@ Class Vehicle {
 
 # arrays
 
+array = Array.new
+# array = []
+
+array_1 = Array.new(1)
+# array_1 = [nil]
+
+array_2 = Array.new(2, 10)
+# array_2 = [10,10]
+
+
+
 names = Array.new
 names = Array.new(20)
 names = Array.new(20)
@@ -221,10 +232,139 @@ puts n.pack("ccc")      #=> "ABC"
 
 
 
+# return, access from end, use -ve indexes
+array[-1]
+# return 1st element
+array.first
+# return last element
+array.last
+# return 1st 3 elements
+array.take(3)
+# return not 1st 3 elements
+array.drop(3)
+
+
+
+# added at the beginning of the list
+x = [4, 5, 6]
+x.unshift(1, 2, 3)
+# => [1, 2, 3, 4, 5, 6]
+
+x = [1,2]
+x.push(3)
+# [1,2,3]
+
+x = [12,19]
+x.insert(1, 15, 16, 17)
+# [12, 15, 16, 17, 19]
+
+def end_arr_add(arr, element)
+    # Add `element` to the end of the Array variable `arr` and return `arr`
+    arr.push(element)
+end
+
+def begin_arr_add(arr, element)
+    # Add `element` to the beginning of the Array variable `arr` and return `arr`
+  arr.unshift(element)
+end
+
+def index_arr_add(arr, index, element)
+    # Add `element` at position `index` to the Array variable `arr` and return `arr`
+  arr.insert(index, element)
+end
+
+def index_arr_multiple_add(arr, index)
+    # add any two elements to the arr at the index
+  arr.insert(index, 1)
+  arr.insert(index, 2)
+end
+
+def element_at(arr, index)
+    # return the element of the Array variable `arr` at the position `index`
+    # arr.at(index) # or
+    # arr[index]
+  
+  arr[index]
+  
+end
+
+def inclusive_range(arr, start_pos, end_pos)
+    # return the elements of the Array variable `arr` between the start_pos and end_pos (both inclusive)
+  
+    arr[start_pos .. end_pos]
+
+end
+
+def non_inclusive_range(arr, start_pos, end_pos)
+    # return the elements of the Array variable `arr`, start_pos inclusive and end_pos exclusive
+  
+      arr[start_pos ... end_pos]
+
+end
+
+def start_and_length(arr, start_pos, length)
+    # return `length` elements of the Array variable `arr` starting from `start_pos`
+
+  arr[start_pos, length]
+
+end
+
+
+def end_arr_delete(arr)
+    # delete the element from the end of the array and return the deleted element
+    return arr.pop
+
+end
+
+def start_arr_delete(arr)
+    # delete the element at the beginning of the array and return the deleted element
+  
+  return arr.shift
+    
+end
+
+def delete_at_arr(arr, index)
+    # delete the element at the position #index
+  
+  arr.delete_at(index)
+    
+end
+
+def delete_all(arr, val)
+    # delete all the elements of the array where element = val
+
+    arr.delete(val)
+
+end
 
 
 
 
+def serial_average(str)
+  # str = SSS-XX.XX-YY.YY
+  # print str
+  
+  d1 = str[4,5]
+  d2 = str[10,5]
+  
+  # print " d1 "
+  # print d1
+  # print " d2 "
+  # print d2
+  # print " "
+  
+  aa = (d1.to_f + d2.to_f).to_f / 2
+  # print " aa "  
+  # print aa
+  
+  # print " str[0,4] "
+  # print str[0,4]
+  
+  # print "" + str[0,4] + "#{aa.round(2)}"
+  # print " "
+  
+  "" + str[0,4] + "#{aa.round(2)}"
+end
 
 
 
