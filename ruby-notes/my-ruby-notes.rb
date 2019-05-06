af0db1546d1a9a34565b5c580db8b4c90c49e31a
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-06 11:51:56 +0530

    more notes
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

--- ruby-notes/my-ruby-notes.rb
+++ ruby-notes/my-ruby-notes.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/ruby -w
 
+# ruby links
+#   https://www.digitalocean.com/community/tutorials/how-to-work-with-string-methods-in-ruby
+#   https://ruby-doc.org/core-2.2.0/Hash.html
+#   https://ruby-doc.org/core-2.6.3/Enumerable.html#method-i-group_by
+#   https://apidock.com/ruby/Array/length
+#   https://apidock.com/ruby/String/start_with%3F
+#   https://www.tutorialspoint.com/ruby/ruby_strings.htm
+
+
 puts "Hello, Ruby!";
 
 puts "This is main Ruby Program"
