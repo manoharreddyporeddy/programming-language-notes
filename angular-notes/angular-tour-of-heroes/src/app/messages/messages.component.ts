499a03a6a0ce21ac1ac1192f4b2bbe9cc361ac94
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-17 20:26:04 +0530

    message component to display (messagesArray pushed by othres) or clear from/to message service
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

---
+++ angular-notes/angular-tour-of-heroes/src/app/messages/messages.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+  // constructor() { }
+
+  // NOTE: messageService is public to bind to it, in template
+  constructor(public messageService: MessageService) { }
+
+  ngOnInit() {
+  }
+
+}
