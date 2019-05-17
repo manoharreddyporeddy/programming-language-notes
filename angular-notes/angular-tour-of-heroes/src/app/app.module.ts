d44229951c0c7de688ba9373c2d042a99338609f
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-17 20:26:24 +0530

    glue the detail & message component
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

--- angular-notes/angular-tour-of-heroes/src/app/app.module.ts
+++ angular-notes/angular-tour-of-heroes/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
