859a91574125c03892606bf6e4f4092a27254a1f
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-17 11:27:23 +0530

    heroes component
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

---
+++ angular-notes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
