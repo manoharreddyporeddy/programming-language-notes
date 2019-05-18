43b787e6d01ccce1c73b0876b2fdad52150e9aec
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-18 08:32:42 +0530

    import router module for our routes, export router module
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

--- angular-notes/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-notes/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+// import { CommonModule } from '@angular/common';
+
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+  // ,
+  // { path: '', component: HeroesComponent }
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+    // CommonModule ,
+  ],
+  exports: [
+    RouterModule
   ]
+  // declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
