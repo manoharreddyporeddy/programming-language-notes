89d0a78e5d4623090f336a97f647fcd19909a977
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-18 20:46:53 +0530

    add new hero from name, delete hero by id
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

--- angular-notes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-notes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -27,6 +27,21 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero)
+      .subscribe();
+  }
+
   // onSelect(hero: Hero): void {
   //   this.selectedHero = hero;
   // }
