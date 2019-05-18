ba5c9dc8c1f83cf0adae55f41fa39a3f7bc1c069
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-18 14:30:53 +0530

    get hero based on id
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

--- angular-notes/angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-notes/angular-tour-of-heroes/src/app/hero.service.ts
@@ -31,4 +31,10 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
