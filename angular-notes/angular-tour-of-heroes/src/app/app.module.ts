42a264e0e4749362678875bd2c9c59331a09e1c1
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-18 08:31:41 +0530

    add component to app module
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

--- angular-notes/angular-tour-of-heroes/src/app/app.module.ts
+++ angular-notes/angular-tour-of-heroes/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
