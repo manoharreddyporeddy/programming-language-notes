9ae30da19c1daa39c8901eb43f7b0fce30736f9e
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-18 14:33:18 +0530

    add routes to dashboard, detail, and redirect empty path to dashboard
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

--- angular-notes/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-notes/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { CommonModule } from '@angular/common';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
-  // ,
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   // { path: '', component: HeroesComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
+
 ];
 
 @NgModule({
