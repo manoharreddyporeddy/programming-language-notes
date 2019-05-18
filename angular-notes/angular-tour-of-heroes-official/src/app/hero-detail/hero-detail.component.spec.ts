d51b5f4ba7c126368ba7e6eab539bb1e28334936
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-05-18 22:11:59 +0530

    merge, format, remove old
    Signed-off-by: Manohar Reddy Poreddy <manoharporeddy@outlook.com>

---
+++ angular-notes/angular-tour-of-heroes-official/src/app/hero-detail/hero-detail.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { HeroDetailComponent } from "./hero-detail.component";
+
+describe("HeroDetailComponent", () => {
+  let component: HeroDetailComponent;
+  let fixture: ComponentFixture<HeroDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroDetailComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
--- angular-notes/angular-tour-of-heroes-official/src/app/hero-search/hero-search.component.ts
+++ angular-notes/angular-tour-of-heroes-official/src/app/hero-search/hero-search.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-
 import { Observable, Subject } from "rxjs";
-
 import { debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
 
 import { Hero } from "../hero";
@@ -27,10 +25,8 @@ export class HeroSearchComponent implements OnInit {
     this.heroes$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
       debounceTime(300),
-
       // ignore new term if same as previous term
       distinctUntilChanged(),
-
       // switch to new search observable each time the term changes
       switchMap((term: string) => this.heroService.searchHeroes(term))
     );
