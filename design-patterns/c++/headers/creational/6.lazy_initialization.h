1c47bfd11d3b5b2397e729e515ab171f459b663f
manoharreddyporeddy <manoharporeddy@outlook.com>
2019-05-20 01:42:52 +0530

    lazy initialization

--- design-patterns/c++/headers/creational/6.lazy_initialization.h
+++ design-patterns/c++/headers/creational/6.lazy_initialization.h
@@ -3,56 +3,45 @@
 #include "../common.h"
 
 /* In computer programming,
-    lazy initialization is the
-    tactic of delaying
-    the creation of an object,
-    the calculation of a value, or
-    some other expensive process
-    UNTIL the first time it is needed.
+      lazy initialization is the
+          tactic of delaying
+              the creation of an object,
+              the calculation of a value, or
+              some other expensive process
+                  UNTIL the first time it is needed.
+*/
 
-    // In computer programming, lazy initialization is the tactic of delaying 
-    //		the creation of an object, 
-    //		the calculation of a value, or 
-    //		some other expensive process 
-    //			until the first time it is needed.
-
-    */
 namespace LAZY_INITIALIZATION {
 
 class Object {
-  // 100 fields
+  // assume 100 fields here
 };
-
-// NOTE
-//		Looks like it doesnt mean Singleton, it sounds more like dynamic variable instead of atuomatic variable (C/C++)
 
 class LazyInitialization {
   static int _count;
 
   public:
-  static int getCount() {
-    cout << "\n .. LazyInitialization class is in use .. ";
-
-    return _count;
-  }
   static Object *createObjectInstance() {
-    Object *p = new Object();
-
     cout << "\n .. Now, LazyInitialization class, is used to create New object .. ";
     _count++;
-
-    return p;
+    return new Object();
   }
+
   // implement call through atexit() for automatic delete?
   static void disposeObjectInstance(Object **pObjectInstance) {
     if (*pObjectInstance != NULL) {
-      /// cout << "\n .. object at address .. " << *pObjectInstance;
+      cout << "\n .. object at address .. " << *pObjectInstance << " is deleted";
+
+      _count--;
 
       delete *pObjectInstance;
       *pObjectInstance = NULL;
-
-      // cout << " is deleted";
     }
+  }
+
+  static int getCount() {
+    cout << " .. LazyInitialization class is in use .. ";
+    return _count;
   }
 };
 int LazyInitialization::_count = 0;
