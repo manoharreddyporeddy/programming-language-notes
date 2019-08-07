883a6f6ecbfafa5db955b0588b5fb0c1cf03ec32
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-08-07 17:27:01 +0530

    Create 03.py

---
+++ my-py/03.py
@@ -0,0 +1,45 @@
+
+a = 5
+b = 2
+
+print (a + b)  # 7
+print (a - b)  # 3
+print (a * b)  # 10
+print (a / b)  # 2.5
+print (a // b) # 2		Floor division (//)	
+
+print (a % b)  # 1 		Modulus (%)	or reminder	
+print (a ** b) # 32 = 2*2* 2*2* 2      Exponentiation (**)		x power y
+
+print (a == b) # False
+
+print (a>0) and (b>0)  # True
+
+
+x = a
+print(x) # 5
+x = x + 2
+print(x) # 7
+x += 2
+print(x) # 9
+
+
+x = "abc"
+print ("a" in x) # True
+print ("z" in x) # False
+
+
+if x is "abc":
+	print ("yes, x is abc")
+else:
+	print ("no, x is not abc")
+
+
+y = 5
+if y > 0:
+	print ("positive")
+elif y < 0:
+	print ("negative")
+else:
+	print (" is 0")
+
