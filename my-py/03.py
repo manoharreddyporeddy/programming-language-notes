c40052f629792b643ea6da53e13c320ea65f116d
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2019-08-09 15:34:27 +0530

    Update 03.py

--- my-py/03.py
+++ my-py/03.py
@@ -35,6 +35,30 @@ else:
 	print ("no, x is not abc")
 
 
+x = 'apple'
+if x in [ "apple", 'banana', 'somethingelse' ]:
+	print ("yes, x is in list")
+else:
+	print ("yes, x is Not in list")
+
+
+y = 5
+if y > 0:
+	print ("positive")
+else:
+	print ("not positive")
+
+
+y = 5
+if y > 0:
+	print ("positive")
+else:
+	if y < 0:
+		print ("negative")
+	else:
+		print (" is 0")
+
+
 y = 5
 if y > 0:
 	print ("positive")
@@ -43,3 +67,40 @@ elif y < 0:
 else:
 	print (" is 0")
 
+# if (condition): statement 1; statement 2; statement 3;…;statement n
+# elif (condition): statement 1; statement 2; statement 3;…;statement n
+# else: statement 1; statement 2; statement 3;…;statement n
+
+x = 69
+if (x >= 60 and x < 70):
+	print ("1st class")
+
+fruitlist = [ "apple", 'banana', 'three', 'four', 'five' ]
+
+print (0)
+print (fruitlist[0])
+
+# fruit will take each of the list values
+for fruit in fruitlist:
+	print (fruit)
+
+
+numOfFruits = len(fruitlist)
+indexlist = range(numOfFruits) # [0,1,2]
+
+print ("")
+for i in indexlist:
+	if i != 1  and  i != 2:
+		print (fruitlist[i])
+
+print ("")
+for i in indexlist:
+	if i == 1  or  i == 2:
+		continue
+	print (fruitlist[i])
+
+print ("")
+for i in indexlist:
+	if i == 2:
+		break
+	print (fruitlist[i])
